99b7d5aa9ea0736362fa61e5e6a68b0f4c727e92
HDCOLOMBIA <jdcastro08@misena.edu.co>
2022-07-10 17:59:53 -0500

    hipotensa

--- hipotenusa.py
+++ hipotenusa.py
@@ -2,8 +2,9 @@
 
 import math
 
-catetoa = int(input("Ingrese valor catetoa"))
-catetoa = int(input("Ingrese valor catetob"))
-raiz_cuadrada = math.sqrt()
+catetoa = int(input("Ingrese valor catetoa: \n"))
+catetob = int(input("Ingrese valor catetob: \n"))
 
+raiz_cuadrada = math.sqrt(catetoa**2 + catetob**2)
 
+print(raiz_cuadrada)
