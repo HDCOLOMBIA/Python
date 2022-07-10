b4c9f77965976fb370647af0f4af59cf3d2b8f30
HDCOLOMBIA <jdcastro08@misena.edu.co>
2022-07-10 11:30:57 -0500

    Revert "fibonasi"
    This reverts commit f2d6d63b487a4c6257d0bdc112ddc0038b3ea354.

--- hipotenusa.py
+++ hipotenusa.py
@@ -1,12 +1,9 @@
 #Hallar la hipotenusa de un tríangulo
 
 import math
-from math import sqrt
 
-catA = float(input("Ingrese valor cateto A: " + "\n"))
-catB = float(input("Ingrese valor cateto B: " + "\n"))
+catetoa = int(input("Ingrese valor catetoa"))
+catetoa = int(input("Ingrese valor catetob"))
+raiz_cuadrada = math.sqrt()
 
-hipotenusa = sqrt(catA**2 + catB**2)
-
-print(f"El valor de la hipotenusa del tríangulo es: \n {hipotenusa}")
 
