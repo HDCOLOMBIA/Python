42953f78d6ef119381793fae7ecd764167953919
HDCOLOMBIA <jdcastro08@misena.edu.co>
2022-07-20 20:51:47 -0500

    prueba

--- reto1robin.py
+++ reto1robin.py
@@ -3,5 +3,6 @@ def nota_quices(codigo: str, nota1: int, nota2: int, nota3: int, nota4: int, not
     promedio = ((nota1 + nota2 + nota3 + nota4 + nota5) - promedio) / 4 * 5 / 100
     promedio = round(promedio, 2)
     return (f"El promedio del estudiante {codigo} es: {promedio}")
+    suma = (nota1 + nota2 + nota3 +)
 
-print(nota_quices("AA0010276", 40, 50, 39, 76, 96))
+print(nota_quices("AA0010276", 40, 50, 39, 76, 96)) 
